818cabdd2eb6ad8bda025ee5c5a69cb4bb5e72ce
Travis Salad <travissalad@gmail.com>
2016-02-25 20:59:49 -0800

    added containers, centered contact form, modified jquery validate

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,10 @@
-jQuery.validator.addMethod("lettersonly", function(value, element){
+/*jQuery.validator.addMethod("lettersonly", function(value, element){
   return this.optional(element) || /^[a-z\s]+$/i.test(value);
 });
 $(document).on('ready', function(){
   $("#contact-form").validate({
+    errorClass: "my-error-class",
+    validClass: "my-valid-class",
     submitHandler: function(form){
       form.submit();
     },
@@ -16,7 +18,7 @@ $(document).on('ready', function(){
         required: true,
         email: true
       },
-      "your-message" {
+      "your-message": {
         required: false,
         maxLength: 500
       },
@@ -27,4 +29,31 @@ $(document).on('ready', function(){
       "your-message": "500 character max",
     },
   });
+});*/
+$(document).on('ready', function(){
+$("#contact-form").validate({
+  submitHandler: function(form){
+    form.submit();
+  },
+  rules: {
+    "your-name": {
+      required: true,
+      maxLength: 150
+    },
+    "your-email": {
+      required: true,
+      email: true
+    },
+    "your-message": {
+      required: true,
+      maxLength: 500
+    },
+  },
+  messages: {
+    "your-name": "Please enter your first and last name",
+    "your-email": "Please enter a valid email address",
+    "your-message": "500 character max",
+  },
+
+});
 });

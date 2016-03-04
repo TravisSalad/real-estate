1bd5175b392a590a9082a3e4d0edae2a38621cb9
Travis Salad <travissalad@gmail.com>
2016-03-03 18:07:27 -0800

    added zillowApi function to pull mortgage data, styled page, added img to about page

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,42 @@
 $(document).on('ready', function(){
-var $animation_elements = $('.animation-element');
-var $window = $(window);
 
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
-
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
+function zillowApi(state){  //create a function that takes on state abbreviation and retrieves zillow mortgage data
+  console.log('meoq');
+  $.ajax({  //make ajax request to zillow
+    type: 'GET',
+    url: 'http://www.zillow.com/webservice/GetRateSummary.htm?zws-id=X1-ZWz1f6abi6ygaz_4wzn5&output=json',
+    data: {state: state},
+    dataType: 'jsonp',
+    success: function(data) {
+      console.log(data);  //show rates for 15 + 30 yr mortgage for today and last week
+      $("#show-rate").text("Today fifteen year: " + data.response.today.fifteenYearFixed);
+      $("#show-rate-2").text("Today thirty year: " + data.response.today.thirtyYearFixed);
+      $("#show-rate-3").text("Last week fifteen year: " + data.response.lastWeek.fifteenYearFixed);
+      $("#show-rate-4").text("Last week thirty year: " + data.response.lastWeek.thirtyYearFixed);
+    },
+    error: function(data, blah, error) {
+      console.log('error: ' + error);
     }
   });
 }
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+
+$("#search").on('click', function() {   //when search button is clicked pass #select-state value to zillowApi function
+  zillowApi($("#select-state").val())
+  event.preventDefault();
 });
 
-
-$(document).on('ready', function(){
-$("#contact-form").validate({
+$("#contact-form").validate({   //validate contact-form
   submitHandler: function(form){
     form.submit();
   },
-  messages: {
+  messages: {   //display error messages
     "your-name": "Please enter your first and last name.",
     "your-email": "Please enter a valid email address (example@example.com)",
     "your-message": "500 character max.",
   }
 });
+
+
+
 });
